85afbcaf1ed8faebcca35fc556f9e5bca6e3571d
Peter Zingg <peter@zingg.name>
2008-05-12 17:16:51 -0700

    not enrolling

--- data/custom/web_root/scripts/jquery_status.js
+++ data/custom/web_root/scripts/jquery_status.js
@@ -1,5 +1,10 @@
 // intent to return form
 $(document).ready(function() {
+	var enrollment = $("#reg_enroll").val();
+	var not_enrolling = (enrollment.indexOf("nr-") == 0);
+	if (not_enrolling) { 
+		$(".enrolling").hide();
+	}
 	$(".chk_status").each(function() {
 		$(this).bind("click", function() { return false; } );
 		var src = "#" + $(this).attr('id').replace('_status', '_updated_at');
