95256d1272e4ac8d466d7a841a3ce3a39a5c7963
Peter Zingg <peter@zingg.name>
2009-01-16 12:16:15 -0800

    change logic

--- data/custom/web_root/scripts/jquery_dialer.js
+++ data/custom/web_root/scripts/jquery_dialer.js
@@ -104,7 +104,7 @@ function on_contact_change(idx) {
       changed = 1;
     }
   }
-  alert("occ " + idx + "0:" + sels[0] + " 1:" + sels[1] + " 2:" + sels[3] + "changed: " + changed);
+  alert("occ(" + idx + ") 0:" + sels[0] + " 1:" + sels[1] + " 2:" + sels[2] + " changed: " + changed);
   if (changed != 0) {
     for (i = idx; i < 3; i++) {
       var options = ParentPhones.get_options(i, sels);
