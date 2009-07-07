d459b3ab3c65b233943bbcb30426bf1e8a90f38d
Peter Zingg <peter@zingg.name>
2009-07-06 17:12:45 -0700

    jquery_form

--- data/custom/web_root/scripts/jquery_guardian.js
+++ data/custom/web_root/scripts/jquery_guardian.js
@@ -1,6 +1,1 @@
-jQuery(document).ready(function() {
-  // blank contents have &nbsp; in them; how do I search for these?
-  // :contains('&nbsp;') doesn't work
-  jQuery(".hideblank .contents").each( function() {
-	  if (jQuery(this).is(":not(:contains('@'))")) { jQuery(this).parent().hide(); } });
-});
+// not used
