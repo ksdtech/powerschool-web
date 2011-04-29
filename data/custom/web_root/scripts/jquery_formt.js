34d206736a68c98f2e432768464d9657c9c04262
Peter Zingg <peter@zingg.name>
2011-04-29 11:26:21 -0700

    teacher contactinfo script

---
+++ data/custom/web_root/scripts/jquery_formt.js
@@ -0,0 +1,82 @@
+
+// happy.js validations
+jq15(document).ready(function () {
+  jq15('#formt').isHappy({
+    // submitButton: jq15('#attSubmitButton'),
+    onSubmit: onRegFormSubmit,
+    fields: {
+      '#street': {
+        required: true,
+        message: 'Required field.' },
+      '#city': { 
+        required: true,
+        message: 'Required field.' },
+      '#state': {
+        required: true,
+        clean: setCAIfBlank,
+        test: happy.USState,
+        message: 'Required field: format as "CA".' },
+      '#zip': { 
+        required: true,
+        test: happy.USZip,
+        message: 'Required field: format as "94904" or "94904-0001"' },
+      '#email_addr': { 
+        required: 'sometimes',
+        test: happy.emptyOrEmail,
+        message: 'Must be a valid email address.' },
+      '#email_personal': { 
+        required: 'sometimes',
+        test: happy.emptyOrEmail,
+        message: 'Must be a valid email address.' },
+      '#home_phone': { 
+        required: true,
+        clean: reformatPhone415,
+        test: happy.USPhoneWithExtension,
+        message: 'Required field: format as (415) 333-2222 x5555.' },
+      '#cell': {
+        required: 'sometimes',
+        clean: reformatPhone415,
+        test: happy.emptyOrUSPhoneWithExtension,
+        message: 'Format as (415) 333-2222 x5555.' },
+      '#summer_phone': {
+        required: 'sometimes',
+        clean: reformatPhone415,
+        test: happy.emptyOrUSPhoneWithExtension,
+        message: 'Format as (415) 333-2222 x5555.' },
+      '#summer_email': { 
+        required: 'sometimes',
+        test: happy.emptyOrEmail,
+        message: 'Must be a valid email address.' },
+      '#emerg1_phone': {
+        required: 'sometimes',
+        clean: reformatPhone415,
+        test: happy.emptyOrUSPhoneWithExtension,
+        message: 'Format as (415) 333-2222 x5555.' },
+      '#emerg1_phone2': {
+        required: 'sometimes',
+        clean: reformatPhone415,
+        test: happy.emptyOrUSPhoneWithExtension,
+        message: 'Format as (415) 333-2222 x5555.' },
+      '#emerg2_phone': {
+        required: 'sometimes',
+        clean: reformatPhone415,
+        test: happy.emptyOrUSPhoneWithExtension,
+        message: 'Format as (415) 333-2222 x5555.' },
+      '#emerg2_phone2': {
+        required: 'sometimes',
+        clean: reformatPhone415,
+        test: happy.emptyOrUSPhoneWithExtension,
+        message: 'Format as (415) 333-2222 x5555.' },
+      '#doctor_phone': {
+        required: 'sometimes',
+        clean: reformatPhone415,
+        test: happy.emptyOrUSPhoneWithExtension,
+        message: 'Format as (415) 333-2222 x5555.' },
+      '#medical_phone': {
+        required: 'sometimes',
+        clean: reformatPhone415,
+        test: happy.emptyOrUSPhoneWithExtension,
+        message: 'Format as (415) 333-2222 x5555.' }
+    }
+  });
+});
