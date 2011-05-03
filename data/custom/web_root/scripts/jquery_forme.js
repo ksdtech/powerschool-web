81e3a5071b6c85f0f62bd926e635e74850980384
Peter Zingg <peter@zingg.name>
2011-05-03 16:11:33 -0700

    Kent enrichment choices

--- data/custom/web_root/scripts/jquery_forme.js
+++ data/custom/web_root/scripts/jquery_forme.js
@@ -1,19 +1,27 @@
+function enrollment_setup() {
+	var enrollment = jq15("#reg_enroll").val();
+	var not_enrolling = (enrollment != null && /^nr-/.test(enrollment));
+	if (not_enrolling) { 
+		jq15("tr.enrolling").hide(); 
+		jq15("tr.not_enrolling").show();
+	} else { 
+		jq15("tr.enrolling").show(); 
+		jq15("tr.not_enrolling").hide();
+		grade_level_setup();
+	}
+	return true;
+}
+
+function grade_level_setup() {
+	jq15("tr.by_grade").hide();
+	var grade = jq15("#reg_grade_level").val();
+	if (grade != "" && grade != "-1") {
+		jq15("tr.by_grade").filter(".grade_" + grade).show();
+	}
+	return true;
+}
+
 // happy.js validations
 jq15(document).ready(function () {
-  jq15('#form1').isHappy({
-    // submitButton: jq15('#attSubmitButton'),
-    fields: {
-      '#reg_enroll': {
-        required: true,
-        message: 'Required field: whether this student is re-enrolling or not.' },
-      '#reg_exitcode': { 
-        required: 'sometimes',
-        test: required_if_not_returning,
-        message: 'Please choose a reason from the list.' },
-      '#reg_exitcomment': { 
-        required: 'sometimes',
-        test: required_if_transfering,
-        message: "Please give the name of the school, or type \"Don\'t know\"." }
-    }
-  });
+  enrollment_setup();
 });
