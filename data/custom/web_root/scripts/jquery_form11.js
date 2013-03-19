96bf4c52a25f6afd209ccb1fd4edf5e482a6595b
Peter Zingg <pzingg@kentfieldschools.org>
2013-03-19 10:53:51 -0700

    kikdir custom fields

--- data/custom/web_root/scripts/jquery_form11.js
+++ data/custom/web_root/scripts/jquery_form11.js
@@ -1,5 +1,17 @@
+function displayListing() {
+  var sample_rows = jq15('#listing .sample');
+  var actual_rows = jq15('#listing #sample_last').nextAll();
+  var has_actual = (actual_rows.size() > 1);
+  if (has_actual) {
+    sample_rows.hide();
+  } else {
+    actual_rows.hide();
+  }
+}
+
 // happy.js validations
 jq15(document).ready(function () {
+  displayListing();
   jq15('#form10').isHappy({
     // submitButton: jq15('#attSubmitButton'),
     onSubmit: onRegFormSubmit,
