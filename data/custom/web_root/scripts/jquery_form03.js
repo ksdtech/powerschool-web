24c956e17f342434b745b279bc4a8adcfb6c2f1a
Peter Zingg <pzingg@kentfieldschools.org>
2014-04-03 13:22:00 -0700

    03-student

---
+++ data/custom/web_root/scripts/jquery_form03.js
@@ -0,0 +1,68 @@
+function required_if_north_american(val) {
+	var birth_country = $j("#birth_country option:selected").val();
+	var error = false
+	if (birth_country != null && /^US|CA|MX$/.test(birth_country)) {
+	  error = (val.length === 0);
+	}
+	return !error;
+}
+
+
+// happy.js validations
+$j(document).ready(function () {
+  $j('#form03').isHappy({
+    // submitButton: $j('#attSubmitButton'),
+    onSubmit: onRegFormSubmit,
+    fields: {
+      '#dob': {
+        required: true,
+        test: happy.date,
+        message: 'Required field: please enter in format M/D/YYYY.' },
+      '#gender': { 
+        required: true,
+        message: 'Please choose a gender from the list.' },
+      '#birth_city': { 
+        required: true,
+        message: 'Required field: birth place city.' },
+      '#birth_state': { 
+        required: 'sometimes',
+        test: required_if_north_american,
+        message: 'Required field for US, Canada or Mexico birth place: state or province.' },
+      '#birth_country': { 
+        required: true,
+        message: 'Required field: birth place country.' },
+      '#lives_with_rel': { 
+        required: true,
+        message: 'Required field.' },
+      '.custody_orders': {
+        default_radio: '#co_no',
+        required: true,
+        message: 'Required field.' },
+      '#first_usa_school': {
+        required: 'sometimes',
+        test: happy.emptyOrDate,
+        message: 'Please enter in format M/D/YYYY.' },
+      '.immigrant': { 
+        required: true,
+        message: 'Required field.' },
+      '.hispanic_ethnicity': { 
+        required: true,
+        message: 'Required field.' },
+      '#parent_edu': {
+        required: true,
+        message: 'Required field.' },
+      '#lang_earliest': {
+        required: true,
+        message: 'Required field.' },
+      '#lang_primary': {
+        required: true,
+        message: 'Required field.' },
+      '#lang_spoken': {
+        required: true,
+        message: 'Required field.' },
+      '#lang_adults': {
+        required: true,
+        message: 'Required field.' }
+    }
+  });
+});
