c8c1c091e90eb910738f81e0214c42347d9ef6c4
Peter Zingg <pzingg@kentfieldschools.org>
2016-01-12 17:42:16 -0800

    pta contact question

--- data/custom/web_root/scripts/jquery_form11.js
+++ data/custom/web_root/scripts/jquery_form11.js
@@ -3,13 +3,6 @@ $j(document).ready(function () {
   $j('#form11').isHappy({
     // submitButton: $j('#attSubmitButton'),
     onSubmit: onRegFormSubmit,
-    fields: {
-      '.pta_contact': {
-        required: true,
-        message: 'Required field.' },
-      '.pta_room_parent': {
-        required: true,
-        message: 'Required field.' }
-    }
+    fields: { }
   });
 });
